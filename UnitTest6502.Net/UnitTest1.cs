89b1bdd62fe64017d90cd545a637d0bd84388bb2
informedcitizenry <informedcitizenry@gmail.com>
2017-04-20 22:19:43 -0700

    Add files via upload

--- UnitTest6502.Net/UnitTest1.cs
+++ UnitTest6502.Net/UnitTest1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Asm6502.Net;
 
@@ -370,7 +372,7 @@ namespace UnitTest6502.Net
         [TestMethod]
         public void TestLittleEndian()
         {
-            Compilation output = new Compilation();
+            Compilation output = new Compilation(true);
 
             output.Add(0xffd2, 2);
             Assert.AreEqual(0x0002, output.GetPC());
@@ -378,6 +380,55 @@ namespace UnitTest6502.Net
             var bytes = output.GetCompilation();
             Assert.AreEqual(0xd2, bytes[0]);
             Assert.AreEqual(0xff, bytes[1]);
+        }
+
+        [TestMethod]
+        public void TestAlignFillRepeat()
+        {
+            Compilation output = new Compilation();
+            output.SetPC(0x4015);
+
+            int foragoodtime = 0xffd220; // for a good time jsr $ffd2
+            int align = output.Align(0x10, foragoodtime); // fill 11 bytes with 0xffd220...
+            Assert.AreEqual(0x4020 - 0x4015, align);
+
+            var bytes1 = output.GetCompilation();
+            Assert.AreEqual(bytes1.Count, align);
+
+            var expected1 = new byte[] { 0x20, 0xd2, 0xff, 
+                                         0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff,
+                                         0x20, 0xd2 };
+
+            Assert.IsTrue(expected1.SequenceEqual(bytes1));
+
+            output.Reset();
+            output.Fill(7, foragoodtime, false);
+
+            Assert.AreEqual(0x0007, output.GetPC());
+
+            var bytes2 = output.GetCompilation();
+            var expected2 = new byte[] { 0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff, 0x20 };
+
+            Assert.IsTrue(expected2.SequenceEqual(bytes2));
+
+            output.Reset();
+
+            output.Fill(7, foragoodtime, true);
+
+            Assert.AreEqual(0x0015, output.GetPC());
+
+            var bytes3 = output.GetCompilation();
+            var expected3 = new byte[] { 0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff,
+                                         0x20, 0xd2, 0xff };
+
+            Assert.IsTrue(expected3.SequenceEqual(bytes3));            
         }
 
         [TestMethod]
