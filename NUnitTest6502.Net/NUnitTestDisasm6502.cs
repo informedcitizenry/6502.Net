bdbe36b117c394b1248c8f2a65ed4cc5486a71e4
informedcitizenry <informedcitizenry@gmail.com>
2017-04-25 16:16:42 -0700

    Add files via upload

---
+++ NUnitTest6502.Net/NUnitTestDisasm6502.cs
@@ -0,0 +1,317 @@
+﻿using NUnit.Framework;
+using Asm6502.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest6502.Net
+{
+
+    [TestFixture]
+    public class DisasmTestVerbose
+    {
+        private IAssemblyController test_;
+        private ILineDisassembler disasm_;
+
+        public DisasmTestVerbose()
+        {
+            test_ = new TestController();
+
+
+            test_.Assemble(new string[] { "--verbose-asm" });
+            disasm_ = new Disasm6502(test_);
+        }
+
+        [Test]
+        public void TestVerboseDisasmSimple()
+        {
+            SourceLine line = new SourceLine();
+            line.SourceString = "    ldx #$00 ; comment here!";
+            line.Instruction = "ldx";
+            line.Operand = "#$00";
+            line.Assembly.AddRange(new byte[] { 0xa2, 0x00 });
+            line.Disassembly = "ldx #$00";
+            line.PC = 0xc025;
+            line.Filename = "testmacro.a65";
+            line.LineNumber = 30;
+
+            string disasm = disasm_.DisassembleLine(line);
+
+            string expected = @"testmacro.a65(30)   :.c025   a2 00       ldx #$00        " +
+                                line.SourceString +
+                                Environment.NewLine;
+
+            Assert.AreEqual(expected, disasm);
+        }
+
+        [Test]
+        public void TestVerboseDisasmEqu()
+        {
+            SourceLine line = new SourceLine();
+            line.SourceString = "               FLAG         =   $34             ";
+            line.Instruction = "=";
+            line.Operand = "$34";
+            line.Disassembly = string.Empty;
+            line.PC = 0x080b;
+            line.Filename = "testmisc2.a65";
+            line.LineNumber = 73;
+
+            string expected =
+@"testmisc2.a65(73)   :=$34                                               FLAG         =   $34             " + Environment.NewLine;
+            string result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestVerboseDisasmLabel()
+        {
+            SourceLine line = new SourceLine();
+            line.PC = 0x080b;
+            line.Label = "stdout";
+            line.SourceString = "stdout      .block";
+            line.Instruction = ".block";
+            line.Operand = string.Empty;
+            line.Disassembly = string.Empty;
+            line.IsDefinition = true;
+            line.Filename = "testsource.a65";
+            line.LineNumber = 33;
+
+            string expected = "testsource.a65(33)  :                                    stdout      .block" + Environment.NewLine;
+            string result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+
+            line.Label = "thisguy";
+            line.SourceString = "thisguy";
+            line.Instruction = string.Empty;
+            line.LineNumber = 32;
+            
+            expected = "testsource.a65(32)  :                                    thisguy   " + Environment.NewLine;
+            result = disasm_.DisassembleLine(line);
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestVerboseDisasmMultiByteFill()
+        {
+            SourceLine line = new SourceLine();
+            line.Filename = "testdisas.a65";
+            line.LineNumber = 68;
+            line.SourceString = "                            .fill 9*3,%........";
+            line.PC = 0x093e;
+            line.Assembly.AddRange(Enumerable.Repeat(Convert.ToByte(0), 9 * 3));
+            line.Disassembly = string.Empty;
+            line.Instruction = ".fill";
+            line.Operand = "9*3,%........";
+
+            string expected =
+@"testdisas.a65(68)   :>093e   00 00 00 00 00 00 00 00                                 .fill 9*3,%........
+                    :>0946   00 00 00 00 00 00 00 00
+                    :>094e   00 00 00 00 00 00 00 00
+                    :>0956   00 00 00" + Environment.NewLine;
+
+            string result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+
+            line.SourceString = "programdog  .fill 8,0";
+            line.Operand = "8,0";
+            line.PC = 0xc03c;
+            line.LineNumber = 41;
+            line.Filename = "testmacro.a65";
+            line.Assembly.Clear();
+            line.Assembly.AddRange(Enumerable.Repeat(Convert.ToByte(0), 8));
+
+            expected = "testmacro.a65(41)   :>c03c   00 00 00 00 00 00 00 00     programdog  .fill 8,0" + Environment.NewLine;
+
+            result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestVerboseDisasmMultiByteString()
+        {
+            string teststring = "HELLO, WORLD! MY NAME IS OPPENHEIMER I HAVE BECOME DESTROYER OF WORDS!!";
+            var ascbytes = Encoding.ASCII.GetBytes(teststring.ToLower());
+            SourceLine line = new SourceLine();
+            line.SourceString = string.Format("            .string \"{0}\"", teststring);
+            line.Instruction = ".string";
+            line.Operand = string.Format("\"{0}\"", teststring);
+            line.Disassembly = string.Empty;
+            line.Assembly.AddRange(ascbytes);
+            line.Filename = "testmacro.a65";
+            line.LineNumber = 41;
+            line.PC = 0xc03c;
+
+            string expected =
+@"testmacro.a65(41)   :>c03c   68 65 6c 6c 6f 2c 20 77                 .string ""HELLO, WORLD! MY NAME IS OPPENHEIMER I HAVE BECOME DESTROYER OF WORDS!!""
+                    :>c044   6f 72 6c 64 21 20 6d 79
+                    :>c04c   20 6e 61 6d 65 20 69 73
+                    :>c054   20 6f 70 70 65 6e 68 65
+                    :>c05c   69 6d 65 72 20 69 20 68
+                    :>c064   61 76 65 20 62 65 63 6f
+                    :>c06c   6d 65 20 64 65 73 74 72
+                    :>c074   6f 79 65 72 20 6f 66 20
+                    :>c07c   77 6f 72 64 73 21 21" + Environment.NewLine;
+
+            string result = disasm_.DisassembleLine(line);
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    [TestFixture]
+    public class DisasmTest
+    {
+        private IAssemblyController test_;
+        private ILineDisassembler disasm_;
+
+        public DisasmTest()
+        {
+            test_ = new TestController();
+            disasm_ = new Disasm6502(test_);
+
+        }
+
+        [Test]
+        public void TestDisasmMultiByteFill()
+        {
+            SourceLine line = new SourceLine();
+            line.SourceString = "                            .fill 9*3,%........";
+            line.PC = 0x093e;
+            line.Assembly.AddRange(Enumerable.Repeat(Convert.ToByte(0), 9*3));
+            line.Disassembly = string.Empty;
+            line.Instruction = ".fill";
+            line.Operand = "9*3,%........";
+
+            string expected =
+@">093e   00 00 00 00 00 00 00 00                                 .fill 9*3,%........
+>0946   00 00 00 00 00 00 00 00
+>094e   00 00 00 00 00 00 00 00
+>0956   00 00 00" + Environment.NewLine;
+
+            string result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+
+            line.SourceString = "somelabel   .fill 8,0";
+            line.Operand = "8,0";
+            line.PC = 0xc03c;
+            line.Assembly.Clear();
+            line.Assembly.AddRange(Enumerable.Repeat(Convert.ToByte(0), 8));
+
+            expected = ">c03c   00 00 00 00 00 00 00 00     somelabel   .fill 8,0" + Environment.NewLine;
+            result = disasm_.DisassembleLine(line);
+            Assert.AreEqual(expected, result);
+            
+
+            line.SourceString = "somelabel   .fill 9,0";
+            line.Operand = "9,0";
+            line.Assembly.Add(0);
+
+            expected =
+ @">c03c   00 00 00 00 00 00 00 00     somelabel   .fill 9,0
+>c044   00" + Environment.NewLine;
+            result = disasm_.DisassembleLine(line);            
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestDisasmMultiByteString()
+        {
+            string teststring = "HELLO, WORLD! MY NAME IS OPPENHEIMER I HAVE BECOME DESTROYER OF WORDS!!";
+            var ascbytes = Encoding.ASCII.GetBytes(teststring.ToLower());
+            SourceLine line = new SourceLine();
+            line.SourceString = string.Format("            .string \"{0}\"", teststring);
+            line.Instruction = ".string";
+            line.Operand = string.Format("\"{0}\"", teststring);
+            line.Disassembly = string.Empty;
+            line.Assembly.AddRange(ascbytes);
+            line.PC = 0xc03c;
+
+            string expected =
+@">c03c   68 65 6c 6c 6f 2c 20 77                 .string ""HELLO, WORLD! MY NAME IS OPPENHEIMER I HAVE BECOME DESTROYER OF WORDS!!""
+>c044   6f 72 6c 64 21 20 6d 79
+>c04c   20 6e 61 6d 65 20 69 73
+>c054   20 6f 70 70 65 6e 68 65
+>c05c   69 6d 65 72 20 69 20 68
+>c064   61 76 65 20 62 65 63 6f
+>c06c   6d 65 20 64 65 73 74 72
+>c074   6f 79 65 72 20 6f 66 20
+>c07c   77 6f 72 64 73 21 21" + Environment.NewLine;
+
+            string result = disasm_.DisassembleLine(line);
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [Test]
+        public void TestDisasmLabel()
+        {
+            SourceLine line = new SourceLine();
+            line.PC = 0x080b;
+            line.Label = "stdout";
+            line.SourceString = "stdout      .block";
+            line.Instruction = ".block";
+            line.Operand = string.Empty;
+            line.Disassembly = string.Empty;
+            line.IsDefinition = true;
+
+            string expected = "                                    stdout    " + Environment.NewLine;
+            string result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+
+            line.Label = "thisguy";
+            line.SourceString = "thisguy";
+            line.Instruction = string.Empty;
+
+            expected = "                                    thisguy   " + Environment.NewLine;
+            result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestDisasmEqu()
+        {
+            SourceLine line = new SourceLine();
+            line.SourceString = "               FLAG         =   $34 ";
+            line.Instruction = "=";
+            line.Operand = "$34";
+            line.Disassembly = string.Empty;
+            line.PC = 0x080b;
+
+            string expected =
+@"=$34                                               FLAG         =   $34 " + Environment.NewLine;
+            string result = disasm_.DisassembleLine(line);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestDisasmSimple()
+        {
+            SourceLine line = new SourceLine();
+            line.SourceString = "    ldx #$00 ; comment here!";
+            line.Instruction = "ldx";
+            line.Operand = "#$00";
+            line.Assembly.AddRange(new byte[] { 0xa2, 0x00 });
+            line.Disassembly = "ldx #$00";
+            line.PC = 0xc000;
+
+            string disasm = disasm_.DisassembleLine(line);
+
+            string expected = @".c000   a2 00       ldx #$00        " + 
+                                line.SourceString + 
+                                Environment.NewLine;
+
+            Assert.AreEqual(expected, disasm);
+        }
+    }
+}
---
+++ NUnitTest6502.Net/NUnitTestPsuedoOps.cs
@@ -0,0 +1,393 @@
+﻿using NUnit.Framework;
+using Asm6502.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest6502.Net
+{
+    [TestFixture]
+    public class PseudoOpsTest
+    {
+        IAssemblyController test_;
+        ILineAssembler pseudo_;
+
+        public PseudoOpsTest()
+        {
+            test_ = new TestController();
+            pseudo_ = new PseudoOps6502(test_);
+        }
+
+        private void TestInstruction(SourceLine line, int pc, IEnumerable<byte> expected, bool positive = true)
+        {
+            pseudo_.AssembleLine(line);
+            if (positive)
+            {
+                Assert.IsFalse(test_.Log.HasErrors);
+                Assert.AreEqual(pc, test_.Output.GetPC());
+
+                if (expected != null)
+                    Assert.IsTrue(test_.Output.GetCompilation().SequenceEqual(expected));
+                else
+                    Assert.IsTrue(test_.Output.GetCompilation().Count == 0);
+            }
+            else
+            {
+                Assert.IsTrue(test_.Log.HasErrors);
+                
+            }
+            test_.Output.Reset();
+            test_.Log.ClearErrors();
+        }
+
+        [Test]
+        public void TestMultiByte()
+        {
+            SourceLine line = new SourceLine();
+            line.Instruction = ".byte";
+            line.Operand = "$01,$02 , $03, $04, $05";
+            TestInstruction(line, 0x0005, new byte[] { 0x01, 0x02, 0x03, 0x04, 0x05 });
+
+            line.Instruction = ".word";
+            line.Operand = "$01,$02, $1234 , $ffff, $7323";
+            TestInstruction(line, 0x000a, new byte[] { 0x01, 0x00, 0x02, 0x00, 0x34, 0x12,
+                                                       0xff, 0xff, 0x23, 0x73});
+
+            line.Instruction = ".lint";
+            line.Operand = "78332, ?, -33000";
+            TestInstruction(line, 0x0009, new byte[] { 0xfc, 0x31, 0x01,
+                                                       0x00, 0x00, 0x00,
+                                                       0x18, 0x7f, 0xff});
+
+            line.Instruction = ".dword";
+            line.Operand = "1,2,3,$ffd2";
+            TestInstruction(line, 0x0010, new byte[] { 0x01, 0x00, 0x00, 0x00,
+                                                       0x02, 0x00, 0x00, 0x00,
+                                                       0x03, 0x00, 0x00, 0x00,
+                                                       0xd2, 0xff, 0x00, 0x00});
+
+            line.Instruction = ".dint";
+            line.Operand = "? , ?,? ,?";
+            TestInstruction(line, 0x0010, null);
+        }
+
+        [Test]
+        public void TestByteChar()
+        {
+            SourceLine line = new SourceLine();
+            line.Instruction = ".byte";
+            line.Operand = "0,255";
+            TestInstruction(line, 0x0002, new byte[] { 0x00, 0xff });
+
+            line.Operand = "-123";
+            TestInstruction(line, 0, null, false);
+
+            line.Instruction = ".char";
+            line.Operand = "-128,127";
+            TestInstruction(line, 0x0002, new byte[] { 0x80, 0x7f });
+
+            line.Operand = "192";
+            TestInstruction(line, 0, null, false);
+        }
+
+        [Test]
+        public void TestWordSint()
+        {
+            SourceLine line = new SourceLine();
+            line.Instruction = ".sint";
+            line.Operand = "-32768,32767";
+            TestInstruction(line, 0x0004, new byte[] { 0x00, 0x80, 0xff, 0x7f });
+
+            line.Operand = "$ffd2";
+            TestInstruction(line, 0, null, false);
+
+            line.Instruction = ".word";
+            TestInstruction(line, 0x0002, new byte[] { 0xd2, 0xff });
+
+            line.Operand = "-1";
+            TestInstruction(line, 0, null, false);
+
+            line.Instruction = ".addr";
+            TestInstruction(line, 0, null, false);
+
+            line.Operand = "65232 , $c000";
+            TestInstruction(line, 0x0004, new byte[] { 0xd0, 0xfe, 0x00, 0xc0 });
+
+            line.Operand = "$010000";
+            TestInstruction(line, 0, null, false);
+
+            line.Instruction = ".word";
+            TestInstruction(line, 0, null, false);
+
+            line.Instruction = ".sint";
+            TestInstruction(line, 0, null, false);
+        }
+
+        [Test]
+        public void TestDwordDint()
+        {
+            SourceLine line = new SourceLine();
+            line.Instruction = ".dint";
+            line.Operand = int.MinValue.ToString() + ", " + int.MaxValue.ToString();
+            TestInstruction(line, 0x0008, new byte[] { 0x00, 0x00, 0x00, 0x80, 
+                                                       0xff, 0xff, 0xff, 0x7f});
+            line.Operand = uint.MaxValue.ToString();
+            TestInstruction(line, 0, null, false);
+
+            line.Instruction = ".dword";
+            TestInstruction(line, 0x0004, new byte[] { 0xff, 0xff, 0xff, 0xff });
+
+            line.Operand = "-32342";
+            TestInstruction(line, 0, null, false);
+        }
+
+        [Test]
+        public void TestLintLong()
+        {
+            SourceLine line = new SourceLine();
+            line.Instruction = ".lint";
+            line.Operand = Int24.MinValue.ToString() + " , " + Int24.MaxValue.ToString();
+            TestInstruction(line, 0x0006, new byte[] { 0x00, 0x00, 0x80, 
+                                                       0xff, 0xff, 0x7f});
+
+            line.Operand = "16777215";
+            TestInstruction(line, 0, null, false);
+
+            line.Instruction = ".long";
+            TestInstruction(line, 0x0003, new byte[] { 0xff, 0xff, 0xff });
+
+            line.Operand = "$01000000";
+            TestInstruction(line, 0, null, false);
+        }
+
+        [Test]
+        public void TestRta()
+        {
+            SourceLine line = new SourceLine();
+            line.Instruction = ".rta";
+            line.Operand = "$0000";
+            TestInstruction(line, 0x0002, new byte[] { 0xff, 0xff });
+
+            line.Operand = "$ffff";
+            TestInstruction(line, 0x0002, new byte[] { 0xfe, 0xff });
+
+            line.Operand = "?";
+            TestInstruction(line, 0x0002, null);
+        }
+
+        [Test]
+        public void TestEnc()
+        {
+            string teststring = "\"hello, world\"";
+
+            var ascbytes = Encoding.ASCII.GetBytes(teststring.Trim('"'));
+            var petbytes = Encoding.ASCII.GetBytes(teststring.Trim('"').ToUpper());
+            var screenbytes = petbytes.Select(b =>
+                {
+                    if (b >= '@' && b <= 'Z')
+                        b -= 0x40;
+                    return b;
+                });
+
+            SourceLine line = new SourceLine();
+            line.Instruction = ".enc";
+            line.Operand = "none";
+            TestInstruction(line, 0x0000, null);
+
+            line.Instruction = ".string";
+            line.Operand = teststring;
+            TestInstruction(line, ascbytes.Count(), ascbytes);
+
+            line.Instruction = ".enc";
+            line.Operand = "petscii";
+            TestInstruction(line, 0x0000, null);
+
+            line.Instruction = ".string";
+            line.Operand = teststring;
+            TestInstruction(line, petbytes.Count(), petbytes);
+
+            line.Instruction = ".enc";
+            line.Operand = "screen";
+            TestInstruction(line, 0x0000, null);
+
+            line.Instruction = ".string";
+            line.Operand = teststring.ToUpper();
+            TestInstruction(line, screenbytes.Count(), screenbytes);
+
+            line.Instruction = ".enc";
+            line.Operand = "none";
+            TestInstruction(line, 0x0000, null);
+        }
+
+        [Test]
+        public void TestStringFormats()
+        {
+            string teststring = "HELLO, WORLD";
+            var ascbytes = Encoding.ASCII.GetBytes(teststring);
+
+            List<byte> test = new List<byte>();
+
+            SourceLine line = new SourceLine();
+
+            test.Add(0x0c);
+            test.AddRange(ascbytes);
+            test.AddRange(BitConverter.GetBytes(0xffd2).Take(2));
+
+            line.Instruction = ".string";
+            line.Operand = string.Format("12, \"{0}\", $ffd2", teststring);
+            TestInstruction(line, 0x000f, test);
+
+            test.Clear();
+            test.AddRange(BitConverter.GetBytes(-32768).Take(2));
+            test.Add(0x0d);
+            test.AddRange(ascbytes);
+            test.Add(0);
+
+            line.Instruction = ".cstring";
+            line.Operand = string.Format("-32768, 13, \"{0}\"", teststring);
+            TestInstruction(line, test.Count, test);
+
+            test.Clear();
+            test.Add(Convert.ToByte(ascbytes.Count() + 2));
+            test.AddRange(ascbytes);
+
+            int instructionsize = 1     // total operand size byte
+                                  + ascbytes.Count() // string size
+                                  + 2;  // two uninitialized bytes
+              
+
+            line.Instruction = ".pstring";
+            line.Operand = string.Format("\"{0}\", ?, ?", teststring);
+            TestInstruction(line, instructionsize, test);
+
+            test.Clear();
+            test.Add(42);
+            test.Add(0);
+            test.Add(0);
+            test.AddRange(ascbytes);
+            test[test.Count - 1] |= 0x80;
+
+            line.Instruction = ".nstring";
+            line.Operand = string.Format("42, ?, ?, \"{0}\"", teststring);
+            TestInstruction(line, test.Count(), test);
+
+            line.Operand = line.Operand + "$80";
+            Assert.Throws<ExpressionEvaluator.ExpressionException>(() =>
+                TestInstruction(line, 0, null, false));
+
+            line.Operand = string.Format("42, ?, ?, \"{0}\", $80", teststring);
+            TestInstruction(line, 0, null, false);
+
+            test.Clear();
+            test.AddRange(BitConverter.GetBytes(0x0d0d).Take(2));
+            test.AddRange(ascbytes);
+            test = test.Select(b => { b <<= 1; return b; }).ToList();
+            test[test.Count - 1] += 1;
+
+            Assert.IsTrue(test_.Output.GetCompilation().Count == 0);
+
+            line.Instruction = ".lsstring";
+            line.Operand = string.Format("$0d0d, \"{0}\"", teststring);
+            TestInstruction(line, test.Count(), test);
+
+            line.Operand += ",$80";
+            TestInstruction(line, 0, null, false);
+        }
+
+        [Test]
+        public void TestFill()
+        {
+            SourceLine line = new SourceLine();
+
+            line.Instruction = ".byte";
+            line.Operand = "0";
+            pseudo_.AssembleLine(line);
+            Assert.IsFalse(test_.Log.HasErrors);
+            Assert.AreEqual(0x0001, test_.Output.GetPC());
+            Assert.IsTrue(test_.Output.GetCompilation().Count == 1);
+
+            // test uninitialized
+            line.Instruction = ".fill";
+            line.Operand = "10";
+            TestInstruction(line, 0x000b, new byte[] { 0x00 });
+
+            // test uninitialized another way
+            line.Operand = "10, ?";
+            TestInstruction(line, 0x000a, null);
+
+            // test fill bytes
+            line.Operand = "10, $ea";
+            TestInstruction(line, 0x000a, new byte[] { 0xea, 0xea, 0xea, 0xea, 0xea,
+                                                       0xea, 0xea, 0xea, 0xea, 0xea});
+
+            line.Operand = string.Empty;
+            Assert.Throws<InvalidOperationException>(() => TestInstruction(line, 0, null, false));
+
+            line.Operand = "10, $ea, $20";
+            TestInstruction(line, 0, null, false);
+
+        }
+
+        [Test]
+        public void TestAlign()
+        {
+            SourceLine line = new SourceLine();
+
+            line.Instruction = ".byte";
+            line.Operand = "0";
+            pseudo_.AssembleLine(line);
+            Assert.IsFalse(test_.Log.HasErrors);
+            Assert.AreEqual(0x0001, test_.Output.GetPC());
+            Assert.IsTrue(test_.Output.GetCompilation().Count == 1);
+
+            // align to nearest page, uninitialized
+            line.Instruction = ".align";
+            line.Operand = "$100";
+            TestInstruction(line, 0x0100, new byte[] { 0x00 });
+
+            // align to nearest page, uninitialized
+            test_.Output.SetPC(1);
+            line.Operand = "$100, ?";
+            TestInstruction(line, 0x0100, null);
+
+            // align to nearest 0x10, filled
+            test_.Output.SetPC(0x0006);
+            line.Operand = "$10, $ea";
+            TestInstruction(line, 0x0010, new byte[] { 0xea, 0xea, 0xea, 0xea, 0xea,
+                                                       0xea, 0xea, 0xea, 0xea, 0xea});
+
+
+            line.Operand = string.Empty;
+            Assert.Throws<InvalidOperationException>(() => TestInstruction(line, 0, null, false));
+            
+            line.Operand = "$100, $10, $02";
+            TestInstruction(line, 0, null, false);
+        }
+
+        [Test]
+        public void TestRepeat()
+        {
+            SourceLine line = new SourceLine();
+
+            line.Instruction = ".repeat";
+            line.Operand = "4, $ffd220";
+            TestInstruction(line, 0x000c, new byte[] { 0x20, 0xd2, 0xff,
+                                                       0x20, 0xd2, 0xff,
+                                                       0x20, 0xd2, 0xff,
+                                                       0x20, 0xd2, 0xff});
+
+            // can't use repeat for uninitialized
+            line.Operand = "4, ?";
+            TestInstruction(line, 0, null, false);
+
+            // can't have less than two args
+            line.Operand = "4";
+            TestInstruction(line, 0, null, false);
+
+            line.Operand = string.Empty;
+            Assert.Throws<InvalidOperationException>(() => TestInstruction(line, 0, null, false));
+        }
+    }
+}
