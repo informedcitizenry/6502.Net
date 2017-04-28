0a1585ca0f462863dd7dd455ecbd8319f9b558df
informedcitizenry <informedcitizenry@gmail.com>
2017-04-27 20:01:11 -0700

    Update Disasm6502.cs

--- 6502.Net/Disasm6502.cs
+++ 6502.Net/Disasm6502.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // Copyright (c) 2017 informedcitizenry <informedcitizenry@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -24,7 +24,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Asm6502.Net
 {
