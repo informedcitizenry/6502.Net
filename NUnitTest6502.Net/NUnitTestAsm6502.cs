a31d9bb59e5773b13b6f61d6d2780584b7d9430c
informedcitizenry <informedcitizenry@gmail.com>
2017-04-27 20:06:05 -0700

    Update NUnitTestAsm6502.cs

--- NUnitTest6502.Net/NUnitTestAsm6502.cs
+++ NUnitTest6502.Net/NUnitTestAsm6502.cs
@@ -1,10 +1,9 @@
-﻿using NUnit.Framework;
 using Asm6502.Net;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace NUnitTest6502.Net
 {
