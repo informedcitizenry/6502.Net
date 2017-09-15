29fa6530ac80d68469aade77640fcc1170ea0809
informedcitizenry <informedcitizenry@gmail.com>
2017-09-15 13:43:54 -0700

    [ci skip]

---
+++ NUnit.Tests/TestDotNetAsm/NUnitTestOpcode.cs
@@ -0,0 +1,48 @@
+﻿using DotNetAsm;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests.TestDotNetAsm
+{
+    [TestFixture]
+    public class NUnitTestOpcode
+    {
+        [Test]
+        public void TestFormatBuilder()
+        {
+            FormatBuilder builder = new FormatBuilder(@"^#(.+)$()", "#{2}", "${0:x2}", string.Empty, 2, 2, 1, 2, false);
+
+            OperandFormat fmt = builder.GetFormat("#$34");
+            Assert.IsNotNull(fmt);
+            Assert.AreEqual("#${0:x2}", fmt.FormatString);
+            Assert.AreEqual("$34", fmt.Expression1);
+            Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
+
+            builder = new FormatBuilder(@"^\(\s*(.+),\s*x\s*\)$()", "({2},x)", "${0:x2}", string.Empty, 2, 2, 1, 2, false);
+
+            fmt = builder.GetFormat("( ZP_VAR , x)");
+            Assert.IsNotNull(fmt);
+            Assert.AreEqual("(${0:x2},x)", fmt.FormatString);
+            Assert.AreEqual("ZP_VAR ", fmt.Expression1);
+            Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
+
+            builder = new FormatBuilder(@"^(.+)$()", "{2}", "${0:x2}", string.Empty, 2, 2, 1, 2, false, true);
+
+            fmt = builder.GetFormat("($3000)");
+            Assert.IsNotNull(fmt);
+            Assert.AreEqual("(${0:x2})", fmt.FormatString);
+            Assert.AreEqual("($3000)", fmt.Expression1);
+            Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
+
+            fmt = builder.GetFormat("$3000");
+            Assert.IsNotNull(fmt);
+            Assert.AreEqual("${0:x2}", fmt.FormatString);
+            Assert.AreEqual("$3000", fmt.Expression1);
+            Assert.IsTrue(string.IsNullOrEmpty(fmt.Expression2));
+        }
+    }
+}
