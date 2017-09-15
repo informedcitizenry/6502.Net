4c0c51ceebc0acd21657ce06134907c9fd988a7e
informedcitizenry <informedcitizenry@gmail.com>
2017-09-15 14:04:19 -0700

    Add files via upload

---
+++ NUnit.Tests/TestDotNetAsm/NUnitTestCharEncoding.cs
@@ -0,0 +1,23 @@
+﻿using DotNetAsm;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests.TestDotNetAsm
+{
+    [TestFixture]
+    public class NUnitTestCharEncoding
+    {
+        [Test]
+        public void TestMethod()
+        {
+            EncodingTranslator translator = new EncodingTranslator();
+            translator.DefineEncoding("petscii");
+            translator.AddEncodingClass("petscii", "az", "A");
+            translator.AddEncodingClass("petscii", "AZ", "a");
+        }
+    }
+}
