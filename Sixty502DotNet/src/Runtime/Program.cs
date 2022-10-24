c3a04d44fcca0d5572fdd9210a30ccb56caa26b8
informedcitizenry <informedcitizenry@gmail.com>
2022-10-24 14:51:25 -0500

    Removed scratch/test data

--- Sixty502DotNet/src/Runtime/Program.cs
+++ Sixty502DotNet/src/Runtime/Program.cs
@@ -13,15 +13,6 @@ namespace Sixty502DotNet
     {
         static void Main(string[] args)
         {
-            int loops = 0;
-            int a = 9, b = 4, c = 7, d = 1, e = 3;
-            while ((a = 1) == 1 && (b = 2) == 3 && (c = 3) == 3 || (d=4) == 4 || (e=5) == 7)
-            {
-                Console.WriteLine($"{a} {b} {c} {d} {e}");
-                if (++loops > 4)
-                    break;
-            }
-            Console.ReadKey();
             try
             {
                 var assemblyController = new AssemblyController(args);
